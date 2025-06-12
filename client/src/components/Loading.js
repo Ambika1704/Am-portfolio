6da0fa0245189c197c204b9bf168219b8a21fb99
Bharath K <8harath.k@gmail.com>
2025-06-12 16:55:42 +0530

    Added loading animation

---
+++ client/src/components/Loading.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Loading.css';
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-content">
+        <div className="loading-spinner">
+          <div className="spinner-circle"></div>
+          <div className="spinner-circle"></div>
+          <div className="spinner-circle"></div>
+        </div>
+        <h2 className="loading-text">Loading Portfolio</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Loading; 
